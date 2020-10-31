4c9bbb4fe7e64ff0241433d7d05631bcef814b94
Amir <amir.j1883@gmail.com>
2020-10-31 18:58:53 +0330

    add post feature

---
+++ posts/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^create_post', views.create_post),
+    url(r'^delete_post', views.delete_post),
+    url(r'^update_post', views.update_post),
+    url(r'^get_user_posts', views.get_user_posts),
+    url(r'^get_post', views.get_post),
+    url(r'^get_content', views.get_content),
+]
+# if settings.DEBUG:
+#     urlpattern += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
