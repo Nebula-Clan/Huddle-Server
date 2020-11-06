10cf8c660807bd269d521980dd689d277f295f92
Hadi Sheikhi <hadsheikhi@gmail.com>
2020-11-06 21:55:46 +0330

    reimplement likes

--- likes/models.py
+++ likes/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from authentication.models import User
 from posts.models import Post
+from comment.models import Comment
 # Create your models here.
 
 
-class Like(models.Model):
+class PostLike(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = user = models.ForeignKey(User, on_delete=models.CASCADE)
