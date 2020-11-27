c8578e7d9de85ff129c579d3cf5777c8d5a541fa
amirmjafari <amir.j1883@gmail.com>
2020-11-27 21:11:37 +0330

    add update user's information requests

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,11 @@
+
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^public', views.get_public_profile),
-    url(r'^image/set', views.set_profile_image)
+    url(r'^image/set', views.set_profile_image),
+    url(r'^update_picture', views.update_picture),
+    url(r'^update_username', views.update_username),
+    url(r'^update_name', views.update_name)
 ]
