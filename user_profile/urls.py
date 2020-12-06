25a495a0666c67054239cb2ce844e8d9f3b6134c
amirmjafari <amir.j1883@gmail.com>
2020-12-07 00:46:15 +0330

    change update profile request

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^public', views.get_public_profile),
-    url(r'^image/set', views.set_profile_image),
-    url(r'^update_picture', views.update_picture),
-    url(r'^update_username', views.update_username),
-    url(r'^update_name', views.update_name),
-    url(r'^update_password', views.update_password)
+    url(r'^update_profile', views.update_profile)
 ]
