5480aedb1b66bf631e6a992ca9f0b5925e34816c
Hadi Sheikhi <hadsheikhi@gmail.com>
2020-12-11 02:06:59 +0330

    asgi-fix

--- huddle/asgi.py
+++ huddle/asgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import huddle.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huddle.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
   "websocket": #QueryAuthMiddleware(
