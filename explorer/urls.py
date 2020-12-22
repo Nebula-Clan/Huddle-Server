5f401cda310eadd69acc9b0ad462a85e53c09ed0
amirmjafari <amir.j1883@gmail.com>
2020-12-22 21:01:32 +0330

    add post by hashtag

--- explorer/urls.py
+++ explorer/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r"^get_posts", views.get_posts)
+    url(r"^get_posts", views.get_posts),
+    url(r"^posts_by_hashtag", views.posts_by_hashtag)
 
 ]
