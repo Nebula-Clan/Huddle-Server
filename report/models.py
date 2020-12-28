fa942dd269851d47bd282f3bc7fcc46366f6d9e1
amirmjafari <amir.j1883@gmail.com>
2020-12-28 18:03:40 +0330

    modify report

--- report/models.py
+++ report/models.py
@@ -2,6 +2,26 @@ from django.db import models
 from authentication.models import User
 from posts.models import Post
 
+report_subjects = [
+        (1, 'insult'),
+        (2, 'aggressive'),
+        (3, 'resist'),
+    ]
+
 class Reports(models.Model):
     user = models.ForeignKey('authentication.User', on_delete = models.CASCADE)
     post = models.ForeignKey('posts.Post', on_delete = models.CASCADE)
+    description = models.CharField(max_length = 500, null = True)
+
+class ReportSubject(models.Model):
+    text = models.CharField(choices = report_subjects, max_length = 20, null = False)
+
+class PostReport(models.Model):
+    subject = models.ForeignKey('report.ReportSubject', on_delete = models.CASCADE)
+    report = models.ForeignKey('report.Reports', on_delete = models.CASCADE)
+
+def update_report_subjects():
+    for sub in report_subjects:
+        if ReportSubject.objects.filter(text = sub[1]).exists():
+            continue
+        ReportSubject.objects.create(text = sub[1])
--- report/views.py
+++ report/views.py
@@ -3,10 +3,11 @@ from authentication.models import User
 from user_profile.serializers import PublicProfileSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .models import Reports
+from .models import *
 from django.http.response import JsonResponse
 from errors.error_repository import get_error_serialized
 from rest_framework import status
+from .serializers import ReportSerializer
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -14,9 +15,19 @@ def send_report(request):
     user = request.user
 
     post_id = request.data.get('post_id', None)
-    if post_id is None:
-        return JsonResponse({"error" : get_error_serialized(103, 'post_id field is required').data}, status = status.HTTP_400_BAD_REQUEST)
-
+    subjects = request.data.get('subjects', None)
+    description = request.data.get('description', None)
+    
+    if subjects is None or len(subjects) == 0 or post_id is None:
+        return JsonResponse({"error" : get_error_serialized(103, 'post_id, subjects fields is required').data}, status = status.HTTP_400_BAD_REQUEST)
+    
+    subjects_obj = []
+    for subject in subjects:
+        report_subject = ReportSubject.objects.filter(text = subject).first()
+        if report_subject is None:
+            return JsonResponse({"error" : get_error_serialized(100, f'This subject is not allowed: \'{subject}\'').data}, status = status.HTTP_400_BAD_REQUEST)
+        subjects_obj.append(report_subject)
+    
     report_post = Post.objects.filter(id = post_id).first()
     if report_post is None:
         return JsonResponse({"error" : get_error_serialized(100, 'Post not found').data}, status = status.HTTP_404_NOT_FOUND)
@@ -24,9 +35,26 @@ def send_report(request):
     if Reports.objects.filter(user = user, post = report_post).exists():
         return JsonResponse({"error" : get_error_serialized(124).data}, status = status.HTTP_400_BAD_REQUEST)
     
-    Reports.objects.create(user = user, post = report_post)
+    created_report = Reports.objects.create(user = user, post = report_post, description = description)
+    for subject in subjects_obj:
+        PostReport.objects.create(subject = subject, report = created_report)
 
     return JsonResponse({"message" : f"report to post with id:{post_id} sended"})
+
+@api_view(['GET'])
+def post_reports(request):
+    post_id = request.query_params.get('post_id', None)
+    if post_id is None:
+        return JsonResponse({"error" : get_error_serialized(103, 'post_id field is required').data}, status = status.HTTP_400_BAD_REQUEST)
+
+    post = Post.objects.filter(id = post_id).first()
+    if post is None:
+        return JsonResponse({"error" : get_error_serialized(100, 'Post not found').data}, status = status.HTTP_404_NOT_FOUND)
+    
+    all_reports = Reports.objects.filter(post = post)
+
+    return JsonResponse({"reports" : ReportSerializer(all_reports, many = True).data})
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
