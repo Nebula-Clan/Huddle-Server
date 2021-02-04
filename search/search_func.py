f8df7e63478f562674ade192970f682ffff46492
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-02-04 14:24:22 +0330

    fix search bug

--- search/search_func.py
+++ search/search_func.py
@@ -41,24 +41,13 @@ from hashtag.edit_distance import edit_distance
 def search(inp, data):
     inp = inp.lower()
     finded = []
-    # words = re.split('[^A-Za-z0-9]+', inp)
+    words = re.split('[^A-Za-z0-9]+', inp)
     edit_distances = {}
     for exp in data:
         # exp_clean = re.sub('[^A-Za-z0-9]+', '', exp[0]).lower()
         exp_clean = exp[0]
-        # for word in words:
-        #     if word in exp_clean:
-        #         finded.append(exp[1])
-        #         continue
-        #     if len(word) < 5:
-        #         continue
-        #     search_tokens = generate_search_token(word)
-        #     for search_token in search_tokens:
-        #         if search_token in exp_clean:
-        #             finded.append(exp[1])
-        edit_distances[exp[0]] = edit_distance(inp, exp_clean, len(inp), len(exp_clean))
-        if("Apa" in exp[0]):
-            print(f'{exp[0]}, {exp_clean}, {inp}, {edit_distances[exp[0]]}')
+        for word in words:
+            edit_distances[exp[0]] = min(edit_distances[exp[0]], edit_distance(inp, exp_clean, len(inp), len(exp_clean)))
     hashtags = sorted(list(data), key= lambda h: search_key(h, edit_distances))
     result = [h[1] for h in hashtags if edit_distances[h[0]] < len(h[0]) / 2 and edit_distances[h[0]] >= 0]
     return result
