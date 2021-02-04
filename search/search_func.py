d2f34d3f353feebf6ba88f037be275ce1c290841
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-02-04 14:11:25 +0330

    fix search bug

--- search/search_func.py
+++ search/search_func.py
@@ -57,7 +57,7 @@ def search(inp, data):
         #             finded.append(exp[1])
         edit_distances[exp[0]] = edit_distance(inp, exp_clean, len(inp), len(exp_clean))
     hashtags = sorted(list(data), key= lambda h: search_key(h, edit_distances))
-    result = [h for h in hashtags if edit_distances[h[0]] < len(h[0]) and edit_distances[h[0]] >= 0]
+    result = [h[1] for h in hashtags if edit_distances[h[0]] < len(h[0]) and edit_distances[h[0]] >= 0]
     return result
 
 def search_key(h, edit_distances):
