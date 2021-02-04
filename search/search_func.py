bccf397002932149fdf81d1d9f1e9669bdd48d75
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-02-04 14:30:46 +0330

    fix search bug

--- search/search_func.py
+++ search/search_func.py
@@ -36,30 +36,22 @@ def generate_search_token(word):
     search_tokens.extend(dele)
     return search_tokens
 
-from hashtag.edit_distance import edit_distance
+
 # data is a list of tuples serach function search in first elements and return secend elements
 def search(inp, data):
     inp = inp.lower()
     finded = []
     words = re.split('[^A-Za-z0-9]+', inp)
-    edit_distances = {}
     for exp in data:
         exp_clean = re.sub('[^A-Za-z0-9]+', '', exp[0]).lower()
         for word in words:
-            if exp[0] in edit_distances:
-                edit_distances[exp[0]] = min(edit_distances[exp[0]], edit_distance(inp, exp_clean, len(inp), len(exp_clean)))
-            else:
-                edit_distances[exp[0]] = edit_distance(inp, exp_clean, len(inp), len(exp_clean))
-        edit_distances[exp[0]] = edit_distance(inp, exp_clean, len(inp), len(exp_clean))
-        if("Apa" in exp[0]):
-            print(f'{exp[0]}, {exp_clean}, {inp}, {edit_distances[exp[0]]}')
-    hashtags = sorted(list(data), key= lambda h: search_key(h, edit_distances))
-    result = [h[1] for h in hashtags if edit_distances[h[0]] < len(h[0]) / 2 and edit_distances[h[0]] >= 0]
-    return result
-
-def search_key(h, edit_distances):
-    if( h[0] in edit_distances):
-         return edit_distances[h[0]] 
-    else: 
-        edit_distances[h[0]] = -1
-        return -1
+            if word in exp_clean:
+                finded.append(exp[1])
+                continue
+            if len(word) < 5:
+                continue
+            # search_tokens = generate_search_token(word)
+            # for search_token in search_tokens:
+            #     if search_token in exp_clean:
+            #         finded.append(exp[1])
+    return finded
